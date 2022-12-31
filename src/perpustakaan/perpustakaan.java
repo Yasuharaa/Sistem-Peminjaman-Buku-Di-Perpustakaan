318f6cb1f32f903dfeb89d3f631745fc08020fc9
Yasuharaa <83198193+Yasuharaa@users.noreply.github.com>
2022-12-31 13:59:23 +0700

    Add files via upload

---
+++ src/perpustakaan/perpustakaan.java
@@ -0,0 +1,287 @@
+package perpustakaan;
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+public class perpustakaan {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        int pilihan;
+        String kodepegawai = "", pegawai="Tidak ditemukan/Salah input kode", siswa="", nis="", x="", kelas="";
+        String kodebuku="", judul="", pengarang="", penerbit="", halaman="";
+        String[] namapegawai ={"Mubaedah, S.Pd","Asep M.Pd", "Fatech S.Kom"};
+        
+        LocalDate tglpinjam = LocalDate.now();
+        LocalDate btskembali = LocalDate.now().plusDays(7);
+        
+         
+        
+        System.out.println("");
+        System.out.println("------------------------------------------------------");
+        System.out.println("            SELAMAT DATANG DI PERPUSTAKAAN            ");
+        System.out.println("------------------------------------------------------"); 
+        
+        String pinjam = "Y";
+        while (pinjam.toUpperCase().trim().equals("Y")){
+        System.out.println("");
+        System.out.println("Ingin Pinjam Atau Mengembalikan Buku?");
+        System.out.println("1. Pinjam Buku");
+        System.out.println("2. Kembalikan Buku");
+        //Pilihan
+        
+        System.out.print("Masukan Pilihan (1-2) : ");
+        pilihan = in.nextInt();
+        
+        switch(pilihan){
+            case 1:
+                System.out.println("");
+                System.out.println("===MENU PEMINJAMAN BUKU====");
+                System.out.println("");
+                
+                //KODE PEGAWAI
+                while(pegawai.equals("Tidak ditemukan/Salah input kode")){
+                System.out.print("Kode Pegawai : ");
+                kodepegawai = in.next();
+                
+            switch (kodepegawai) {
+                case "101":
+                    pegawai = namapegawai[0];
+                    break;
+                case "102":
+                    pegawai = namapegawai[1];
+                    break;
+                case "103":
+                    pegawai = namapegawai[2];
+                    break;
+                default:
+                    pegawai = "Tidak ditemukan/Salah input kode";
+                    break;   
+            }
+                System.out.println("Nama Pegawai : "+pegawai);
+                System.out.println("---------------------------");
+                }
+                
+                //INPUT DATA SISWA
+                in.nextLine();   
+                System.out.print("Nama Siswa : ");
+                siswa = in.nextLine();
+                System.out.print("Kelas      : ");
+                kelas = in.nextLine();
+                System.out.print("NIS        : ");
+                nis = in.nextLine(); 
+                
+                //KODE BUKU
+                System.out.print("Kode Buku  : ");
+                kodebuku = in.nextLine();
+                System.out.println("");                
+                switch(kodebuku){
+                    case "1001":
+                        judul = "Si Kancil Mencuri Timun";
+                        pengarang ="Tedi Siswoko";
+                        penerbit ="JP Books";
+                        halaman ="56";
+                        break;
+                    case "1002":
+                        judul = "Kisah Kelinci dan kura - kura";
+                        pengarang ="Magda";
+                        penerbit ="PT. Balai Pustaka";
+                        halaman ="30";
+                        break;
+                    case "1003":
+                        judul = "Nama Kamu";
+                        pengarang ="Makoto Shinkai";
+                        penerbit ="Kadokawa";
+                        halaman ="336";
+                        break;
+                    case "1004":
+                        judul = "Merasa Pintar Bodoh Saja Tak Punya";
+                        pengarang ="Rusdi Mathari";
+                        penerbit ="Buku Mojok";
+                        halaman ="226";
+                        break;
+                    case "1005":
+                        judul = "Bahasa Indonesia Kelas X";
+                        pengarang ="Istiqomah";
+                        penerbit ="Kemendikbud";
+                        halaman ="290";
+                        break;
+                    default :
+                        judul=null;
+                        pengarang=null;
+                        penerbit=null;
+                        halaman=null;
+                }
+                
+                
+                
+                
+                //BUKTI PEMINJAMAN
+                System.out.println("");
+                System.out.println("--------------------------------------------------");
+                System.out.println("       PERPUSTAKAAN SMA NEGERI 5 PEKALONGAN       ");
+                System.out.println("            PEMINJAMAN & PENGEMBALIAN             ");
+                System.out.println("                       BUKU                       ");
+                System.out.println("--------------------------------------------------");
+                System.out.println("");
+                System.out.println("=== BUKTI PEMINJAMAM ===");
+                System.out.println("Nama Pegawai  : "+pegawai);
+                System.out.println("Nama Peminjam : "+siswa);
+                System.out.println("NIS           : "+nis);
+                System.out.println("");
+                System.out.println("---Informasi Buku---");
+                System.out.println("Kode Buku          : "+kodebuku);
+                System.out.println("Judul Buku         : "+judul);
+                System.out.println("Pengarang          : "+pengarang);
+                System.out.println("Penerbit           : "+penerbit);
+                System.out.println("Jumlah Halaman     : "+halaman+" Halaman");
+                System.out.println("Tanggal Pinjam     : "+tglpinjam.format(DateTimeFormatter.ofPattern("dd-MMMM-yyyy")));
+                System.out.println("Batas Pengembalian : "+btskembali.format(DateTimeFormatter.ofPattern("dd-MMMM-yyyy")));
+                System.out.println("Catatan 'Jika Anda Melebihi Batas Tanggal Kembali'\nAkan dikenakan DENDA sebesar Rp.500 per Harinya!");
+                System.out.println("");                
+                
+                
+                //Nilai default untuk loop salah input kode pegawai
+                pegawai="Tidak ditemukan/Salah input kode";
+                break;
+            case 2:
+                System.out.println("");
+                System.out.println("===MENU PENGEMBALIAN BUKU====");
+                //KODE PEGAWAI
+                while(pegawai.equals("Tidak ditemukan/Salah input kode")){
+                System.out.print("Kode Pegawai : ");
+                kodepegawai = in.next();
+                
+            switch (kodepegawai) {
+                case "101":
+                    pegawai = namapegawai[0];
+                    break;
+                case "102":
+                    pegawai = namapegawai[1];
+                    break;
+                case "103":
+                    pegawai = namapegawai[2];
+                    break;
+                default:
+                    pegawai = "Tidak ditemukan/Salah input kode";
+                    break;   
+            }
+                System.out.println("Nama Pegawai : "+pegawai);
+                System.out.println("---------------------------");
+                }
+                
+                //INPUT DATA SISWA
+                in.nextLine();   
+                System.out.print("Nama Siswa : ");
+                siswa = in.nextLine();
+                System.out.print("Kelas      : ");
+                kelas = in.nextLine();
+                System.out.print("NIS        : ");
+                nis = in.nextLine(); 
+                
+                //KODE BUKU
+                System.out.print("Kode Buku  : ");
+                kodebuku = in.nextLine();                
+                switch(kodebuku){
+                    case "1001":
+                        judul = "Si Kancil Mencuri Timun";
+                        pengarang ="Tedi Siswoko";
+                        penerbit ="JP Books";
+                        halaman ="56";
+                        break;
+                    case "1002":
+                        judul = "Kisah Kelinci dan kura - kura";
+                        pengarang ="Magda";
+                        penerbit ="PT. Balai Pustaka";
+                        halaman ="30";
+                        break;
+                    case "1003":
+                        judul = "Nama Kamu";
+                        pengarang ="Makoto Shinkai";
+                        penerbit ="Kadokawa";
+                        halaman ="336";
+                        break;
+                    case "1004":
+                        judul = "Merasa Pintar Bodoh Saja Tak Punya";
+                        pengarang ="Rusdi Mathari";
+                        penerbit ="Buku Mojok";
+                        halaman ="226";
+                        break;
+                    case "1005":
+                        judul = "Bahasa Indonesia Kelas X";
+                        pengarang ="Istiqomah";
+                        penerbit ="Kemendikbud";
+                        halaman ="290";
+                        break;
+                    default :
+                        judul=null;
+                        pengarang=null;
+                        penerbit=null;
+                        halaman=null;
+                }
+                System.out.print("Masukan Tanggal Peminjaman : ");
+                x = in.nextLine();
+                LocalDate tglpeminjaman = LocalDate.parse(x, DateTimeFormatter.ofPattern("dd-MMMM-yyyy"));
+                LocalDate btspengembalian = tglpeminjaman.plusDays(7);
+                
+                //BUKTI PENGEMBALIAN
+                System.out.println("");
+                System.out.println("--------------------------------------------------");
+                System.out.println("       PERPUSTAKAAN SMA NEGERI 5 PEKALONGAN       ");
+                System.out.println("            PEMINJAMAN & PENGEMBALIAN             ");
+                System.out.println("                       BUKU                       ");
+                System.out.println("--------------------------------------------------");
+                System.out.println("");
+                System.out.println("=== BUKTI PENGEMBALIAN ===");
+                System.out.println("Nama Pegawai  : "+pegawai);
+                System.out.println("Nama Peminjam : "+siswa);
+                System.out.println("NIS           : "+nis);
+                System.out.println("");
+                System.out.println("---Informasi Buku---");
+                System.out.println("Kode Buku          : "+kodebuku);
+                System.out.println("Judul Buku         : "+judul);
+                System.out.println("Pengarang          : "+pengarang);
+                System.out.println("Penerbit           : "+penerbit);
+                System.out.println("Jumlah Halaman     : "+halaman+" Halaman");
+                System.out.println("Tanggal Pinjam     : "+tglpeminjaman.format(DateTimeFormatter.ofPattern("dd-MMMM-yyyy")));
+                System.out.println("Batas Pengembalian : "+btspengembalian.format(DateTimeFormatter.ofPattern("dd-MMMM-yyyy")));
+                System.out.println("----------------------");
+                System.out.print("Masukan Tanggal Pengembalian : ");
+                x = in.nextLine();
+                LocalDate tglpengembalian = LocalDate.parse(x, DateTimeFormatter.ofPattern("dd-MMMM-yyyy" ));
+                LocalDate telat = LocalDate.from(tglpengembalian).minusDays(btspengembalian.getDayOfYear());
+                //sebelum if aku pakai while XD
+                if(tglpengembalian.getDayOfYear()>btspengembalian.getDayOfYear()){
+                int haritelat = telat.getDayOfYear();
+                
+                System.out.println("telat = "+haritelat+" Hari");
+                System.out.println("Denda diberikan jika melewati batas pengembalian. Per harinya Rp.500");
+                int denda = haritelat*500;
+                
+                System.out.println("Denda yang harus dibayarkan : Rp."+denda);
+                System.out.println(""); 
+                //STOP
+                //Ga tau kenapa kok keprint loop, tolong bagaimana cara menghentikannyaaa
+                //karena pake while kali
+                //ini ganti if else tiba2 WORK HOKI kali wkwkw
+                
+                //Nilai default untuk loop salah input kode pegawai
+                pegawai="Tidak ditemukan/Salah input kode";
+        }else{
+                System.out.println("Anda Mengembalikan Tepat Waktu dan Belum Melewati Batas");
+                System.out.println("telat = 0 Hari");
+                System.out.println("Denda = Rp.0");
+                }
+                break;
+                
+                
+            default :
+                System.out.println("salah input");
+                break;
+            }
+        System.out.println("Apakah Anda ingin Meminjam Lagi? Atau ingin Mengembalikan (Y/T)");
+        pinjam = in.next();
+        }
+        
+    }
+    
+}
